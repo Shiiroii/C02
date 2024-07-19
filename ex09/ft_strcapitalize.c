f196eebdf2784ce54b231968ce9ef4a47c067e15
liulm <lionelulm67@gmail.com>
2024-07-19 11:44:24 +0200

    updated c02

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -6,11 +6,35 @@
 /*   By: liulm <liulm@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 16:44:29 by liulm             #+#    #+#             */
-/*   Updated: 2024/07/18 16:44:42 by liulm            ###   ########.fr       */
+/*   Updated: 2024/07/19 11:43:27 by liulm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strcapitalize(char *str)
 {
-	
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[0] >= 'a' && str[0] <= 'z')
+			str[0] -= 32;
+		if (str[i] == ' ' || str[i] == '-' || str[i] == '+')
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] -= 32;
+		}
+		i++;
+	}
+	return (0);
+}
+
+#include <stdio.h>
+
+int	main()
+{
+	char	str[] = "salut, comment tu vas ? 42mots quarante-deux; cinquante+et+un";
+
+	ft_strcapitalize(str);
+	printf("%s", str);
 }
