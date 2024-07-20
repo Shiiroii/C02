53f9c3b9341a24643cadee745114b9115fe39079
liulm <lionelulm67@gmail.com>
2024-07-20 16:26:26 +0200

    added ex 11

--- ex11/ft_putstr_non_printable.c
+++ ex11/ft_putstr_non_printable.c
@@ -6,11 +6,32 @@
 /*   By: liulm <liulm@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 13:25:20 by liulm             #+#    #+#             */
-/*   Updated: 2024/07/20 14:57:40 by liulm            ###   ########.fr       */
+/*   Updated: 2024/07/20 16:06:58 by liulm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print_in_hexadecimal(int nb)
+{
+	char	*hexa;
+
+	hexa = "0123456789abcdef";
+	if (nb > 16)
+	{
+		print_in_hexadecimal(nb / 10);
+		print_in_hexadecimal(nb % 10);
+	}
+	else
+	{
+		ft_putchar(hexa[nb]);
+	}
+}
 
 void	ft_putstr_non_printable(char *str)
 {
@@ -19,14 +40,28 @@ void	ft_putstr_non_printable(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+		if (str[i] >= 32 && str[i] <= 126)
 		{
-			write(1, &str[i], 1);
+			ft_putchar(str[i]);
 		}
-		if (str[i] >= 0 && str[i] <= 31)
+		if ((str[i] >= 0 && str[i] <= 31) || (str[i] == 127))
 		{
-			
+			ft_putchar('\\');
+			if (str[i] < 16)
+			{
+				ft_putchar('0');
+			}
+			print_in_hexadecimal(str[i]);
 		}
 		i++;
 	}
+}
+
+int	main(void)
+{
+	char	str[] = "Salut \x0A salut \x7F ! \n";
+
+	ft_putstr_non_printable(str);
+	ft_putchar('\n');
+	return (0);
 }
