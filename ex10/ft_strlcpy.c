c75a20de7db72bb179b0d51157fe8b50395bf86b
liulm <lionelulm67@gmail.com>
2024-07-20 13:22:45 +0200

    added ex10

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,11 +6,48 @@
 /*   By: liulm <liulm@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/19 10:35:37 by liulm             #+#    #+#             */
-/*   Updated: 2024/07/19 10:35:56 by liulm            ###   ########.fr       */
+/*   Updated: 2024/07/20 13:21:36 by liulm            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	
+	unsigned int	i;
+
+	i = 0;
+	if (size == 0)
+	{
+		return (ft_strlen(src));
+	}
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (ft_strlen(dest));
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*str;
+
+	str = "Salut Salut Salut";
+	char	a[ft_strlen(str)];
+	ft_strlcpy(a, str, 5);
+	printf("ft_strlcpy : %s\n", a);
 }
